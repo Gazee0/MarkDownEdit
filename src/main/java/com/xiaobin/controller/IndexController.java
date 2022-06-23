cc8acb5107e738ea5bd98b892560e643460a36ca
fcsh <2682582660@qq.com>
2022-06-23 20:13:55 +0800

    测试push代码

---
+++ src/main/java/com/xiaobin/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.xiaobin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("")
+    public void index(){
+
+    }
+}
---
+++ src/main/java/com/xiaobin/md/MarkDownEditApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobin.md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkDownEditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkDownEditApplication.class, args);
+    }
+
+}
